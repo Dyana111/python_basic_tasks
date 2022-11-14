ada11f7de8e250b4aad0ba4f05d02c23b52187c0
Dyana111 <diana.raed92@gmail.com>
2022-11-14 02:09:33 +0100

    operator task

---
+++ 01-variables/tasks.py
@@ -0,0 +1,33 @@
+#1: Create a bool variable named x
+x=false
+#2:create an integer variable named y
+y=30
+#3:create a floate variable named z
+z=3.6
+#4: create a string variable named s
+s='diana'
+t='talya'
+#5:convert the int variable to float
+y=30.3
+#6: can you convert the str to int
+s=30
+t=1.6
+#7: create a list of number from 1 to 5
+l=[1,2,3,4,5]
+#8:create a tuple from 10 to 15
+tu=(10,11,12,13,14,15)
+#9:convert the list to tuple
+l=(1,2,3,4,5)
+#10: create a dict of 3 values:
+d={'raed':33, 'diana':30, 'talya':1.5}
+#11: can we use semi colon with python
+yes
+#12:python is interpreted or compiled
+interpreted
+#13what is the differences between low level and high level
+high level programme languges are easier for the programmers to compiled,
+interprete, or understand then machines. have to use a compiler or an
+interpreter to translate the code to machines
+so the low level languges are easier for machines in comparion of ppl.
+low level languges requier an assembler for directly translating the
+instructions of the machine language.
---
+++ 02-operators/tasks.py
@@ -0,0 +1,30 @@
+#1: what is the difference between = and ==
+=: is to give the variable a value
+==: it means equal
+#2: what do we mean by using !=
+not equal
+#3: what is the operator precedence
+() --> ** --> * / --> + -
+#4: create a variable x with value of 10
+x=10
+#5: increase x value by 15 using assignment operators
+x +=5
+#6: divide the x value by 5 using assignment operators
+x /=5
+#7: multiply the x on 3 using assignment operators
+x *=3
+#8: get module of x on 3 using assignment operators
+x %=3
+#9: using python print the module of 11 to 4
+y=11%4
+print(y) answer: 3
+#10:print the exponent of 2,3
+z=2**3
+print(z) answer: 8
+#11: divide 11 by 3 using python divison
+d= 11/3
+print (d) answer: 3.666666
+#12: can you divide 11 by 3 and get an int number
+d= 11//3
+print (d) answer: 3
+
