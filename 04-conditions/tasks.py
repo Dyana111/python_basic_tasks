4d19b47bb170ee5156c81c4d0135764650236d16
Dyana111 <diana.raed92@gmail.com>
2022-11-17 17:53:21 +0100

    conditions not complete

---
+++ 04-conditions/tasks.py
@@ -0,0 +1,14 @@
+#1:check if 10 is bigger than 15 or not
+x=10
+if(x> 15):
+    print('it is bigger')
+
+else:
+    print("no it's not")
+#2:if 10 is not bigger than 15 print x is smaller than 15
+x=11
+if(x> 15):
+    print('it is bigger')
+
+else:
+    print(" x is smaller than 15")
