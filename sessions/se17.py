c3766939ec2a790c2634da58daa8df0175333572
Dyana111 <diana.raed92@gmail.com>
2022-11-26 14:58:21 +0100

    se. 18

--- sessions/se17.py
+++ sessions/se17.py
@@ -13,7 +13,7 @@ for x in [[10,20,30],[30,40,50],[60,70,80],4,5,6]:
             print (y)
     else:
         print (x)
-'''
+
 
 start= int(input('please enter the start number:'))
 end= int(input('please enter the end number:'))
@@ -22,3 +22,50 @@ for x in range (start, end):
     for y in range (start, end):
         print(f'{x}X{y}= ', x*y)
    
+start= int(input('please enter the start number:'))
+end=int(input('please enter the end number:'))
+print ('Number \t Square')
+print ('---------------------')
+for x in range(start,end+1):
+    print(f'{x} \t {x*x}')
+
+
+row=int(input('enter the starting number:'))
+clos=int(input('How high should i go:'))
+for x in range(row):
+    for y in range(clos):
+        print('*', end='')
+    print('\n')
+
+'''
+rows=8
+for x in range(1, rows+1):
+    for y in range(x):
+        print('*', end='')
+    print(' ')
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
